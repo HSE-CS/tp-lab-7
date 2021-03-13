1d9a483050f877377f385c8daf8c7ee118508307
Ivan Dobryaev <ivan.dobryaev@gmail.com>
2021-03-13 23:05:01 +0300

    final version

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 int main() {
   Ocean *oc = new Ocean;
-  oc->addObjects(700, ObjType::PREY);
-  oc->addObjects(1500, ObjType::PREDATOR);
+  oc->addObjects(1500, ObjType::PREY);
+  oc->addObjects(200, ObjType::PREDATOR);
   oc->addObjects(200, ObjType::STONE);
   oc->run();
   return 0;
