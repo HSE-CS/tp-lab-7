d8fb8fbb836856946189a4dc7f47619d98f7dd1d
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-03-21 18:39:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,17 @@
 // Copyright Baklanov 2021
-#include "cell.h"
+#include "Cell.h"
 #include "Ocean.h"
 #include "Predator.h"
-#include "prey.h"
-#include "stone.h"
+#include "Prey.h"
+#include "Stone.h"
+#include "common.h"
 #include <iostream>
 #include <cstdlib>
 #include <list>
-#include "windows.h"
-#include "common.h"
 
 int main() {
     Ocean a(40, 80);
     a.addObjects(60, 10, 5);
-    HANDLE hd = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO structCursorInfo;
-    GetConsoleCursorInfo(hd, &structCursorInfo);
-    structCursorInfo.bVisible = FALSE;
-    SetConsoleCursorInfo(hd, &structCursorInfo);
-    COORD cd;
-    cd.X = 0;
-    cd.Y = 0;
     int PreyCounter = 0;
     int PredatorCounter = 0;
     for (int i = 0; i < 100; ++i) {
@@ -44,6 +35,5 @@ int main() {
         PredatorCounter = 0;
         PreyCounter = 0;
         Sleep(200);
-        SetConsoleCursorPosition(hd, cd);
     }
 }
