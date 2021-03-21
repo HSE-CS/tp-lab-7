1fd1a5c99cde19bb56711304cedb76abb348c900
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-03-21 19:27:02 +0300

    Update Common.h

--- include/Common.h
+++ include/Common.h
@@ -1,14 +1,12 @@
-﻿  // Copyright Baklanov 2021
+// Copyright Baklanov 2021
 #ifndef INCLUDE_COMMON_H_
 #define INCLUDE_COMMON_H_
-
+#include "Ocean.h"
+#include <string>
 #define STONE_N '#'
 #define CORAL_N '*'
 #define PREY_N 'f'
 #define PREDATOR_N 'S'
-
-#include "Ocean.h"
-#include <string>
 
 enum class ObjType { STONE, CORAL, PREY, PREDATOR };
 enum class Move {UPLEFT, UP, UPRIGHT, RIGHT,
