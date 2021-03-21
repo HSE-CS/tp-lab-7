47bc6551825be6db6ba4e3ed3711ce1c1b40f4ef
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-03-21 19:00:38 +0300

    Update Common.cpp

--- src/Common.cpp
+++ src/Common.cpp
@@ -1,8 +1,8 @@
 // Copyright Baklanov 2021
+#include "Common.h"
 #include "Ocean.h"
 #include "Predator.h"
 #include "Prey.h"
-#include "Common.h"
 #include <iostream>
 
 void Object::Reproduction(Ocean* ocean_) {
