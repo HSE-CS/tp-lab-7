3f6f48cc218e6ad18b6456012f10a330aa3c6b81
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2021-03-23 17:14:05 +0300

    fix typo

--- include/cell.h
+++ include/cell.h
@@ -1,29 +1,29 @@
 // Copyright 2021 Khoroshavina Ekaterina
-#ifndef _CELL_H_
-#define _CELL_H_
-#include "common.h"
-#include "object.h"
-#include "ocean.h"
+#ifndef INCLUDE_CELL_H_
+#define INCLUDE_CELL_H_
+#include "../include/common.h"
+#include "../include/object.h"
+#include "../include/ocean.h"
 
 class Ocean;
 
 class Cell {
-	friend Ocean;
-private:
-	Pair crd;
-	Object* obj;
-	Ocean* ocean;
-public:
-	explicit Cell(Pair p = {0, 0}, Ocean* oc = nullptr) :
-		crd(p),
-		obj(nullptr),
-		ocean(oc) {}
-	void init(Pair p, Ocean* oc);
-	Object* getObject() const;
-	void setObject(Object* obj);
-	void killMe();
-	Ocean* getOcean();
-	Pair getCrd();
-	Cell* makeNewCell();
+ friend Ocean;
+ private:
+    Pair crd;
+    Object* obj;
+    Ocean* ocean;
+ public:
+    explicit Cell(Pair p = {0, 0}, Ocean* oc = nullptr) :
+        crd(p),
+        obj(nullptr),
+        ocean(oc) {}
+    void init(Pair p, Ocean* oc);
+    Object* getObject() const;
+    void setObject(Object* obj);
+    void killMe();
+    Ocean* getOcean();
+    Pair getCrd();
+    Cell* makeNewCell();
 };
-#endif
+#endif // INCLUDE_CELL_H_
