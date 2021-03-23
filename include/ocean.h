a94b276950f850b1df876d1d10a708a2855f5ff4
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2021-03-23 17:17:36 +0300

    fix typo

--- include/ocean.h
+++ include/ocean.h
@@ -1,24 +1,24 @@
 // Copyright 2021 Khoroshavina Ekaterina
-#ifndef _OCEAN_H_
-#define _OCEAN_H_
+#ifndef INCLUDE_OCEAN_H_
+#define INCLUDE_OCEAN_H_
 #include <list>
-#include "object.h"
-#include "cell.h"
-#include "predator.h"
-#include "prey.h"
-#include "stone.h"
+#include "../include/object.h"
+#include "../include/cell.h"
+#include "../include/predator.h"
+#include "../include/prey.h"
+#include "../include/stone.h"
 
 class Ocean {
-private:
-	Cell** cells;
-	std::list<Object*> stuff;
-public:
-	Ocean();
-	~Ocean();
-	void makeOcean(int numPrey, int numPredator, int numStone);
-	void print() const;
-	void addObject(Object* newObj);
-	void run();
-	Cell* newCell(Pair crd);
+ private:
+    Cell** cells;
+    std::list<Object*> stuff;
+ public:
+    Ocean();
+    ~Ocean();
+    void makeOcean(int numPrey, int numPredator, int numStone);
+    void print() const;
+    void addObject(Object* newObj);
+    void run();
+    Cell* newCell(Pair crd);
 };
-#endif
+#endif // INCLUDE_OCEAN_H_
