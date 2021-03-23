7e03ea7e0f50519a15700ceb9eb51e238e771411
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2021-03-23 17:19:56 +0300

    fix typo

--- include/stone.h
+++ include/stone.h
@@ -1,14 +1,14 @@
 // Copyright 2021 Khoroshavina Ekaterina
 #ifndef INCLUDE_STONE_H_
 #define INCLUDE_STONE_H_
-#include "object.h"
-#include "common.h"
+#include "../include/object.h"
+#include "../include/common.h"
 
 class Stone : public Object {
-public:
+ public:
     Stone(ObjType newObjType, Cell* cell);
     ~Stone();
     void live();
 };
 
-#endif
+#endif // INCLUDE_STONE_H_
