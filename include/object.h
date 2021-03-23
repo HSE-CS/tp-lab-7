7340d63d56a0e13b9fb6de2e36b64dd8401f789e
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2021-03-23 17:16:29 +0300

    fix typo

--- include/object.h
+++ include/object.h
@@ -1,7 +1,7 @@
 // Copyright 2021 Khoroshavina Ekaterina
-#ifndef _OBJECT_H_
-#define _OBJECT_H_
-#include "common.h"
+#ifndef INCLUDE_OCEAN_H_
+#define INCLUDE_OCEAN_H_
+#include "../include/common.h"
 
 #define STONE_N '#'
 #define PREY_N 'f'
@@ -11,19 +11,19 @@ enum class ObjType { STONE, EMPTY, PREY, PREDATOR };
 
 class Cell;
 class Object {
-protected:
-	Cell* cell;
-	ObjType objType;
-	bool alive;
-	int hp;
-public:
-	Object(ObjType newObjType, Cell* cell = nullptr);
-	virtual ~Object();
-	virtual void live() = 0;
-	void setCell(Cell*);
-	void death();
-	Cell* getCell();
-	ObjType getType();
-	bool isAlive() const;
+ protected:
+    Cell* cell;
+    ObjType objType;
+    bool alive;
+    int hp;
+ public:
+    explicit Object(ObjType newObjType, Cell* cell = nullptr);
+    virtual ~Object();
+    virtual void live() = 0;
+    void setCell(Cell*);
+    void death();
+    Cell* getCell();
+    ObjType getType();
+    bool isAlive() const;
 };
-#endif
+#endif // INCLUDE_OCEAN_H_
