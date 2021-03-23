f2856eed385a1f2647090964f5ca114281ad3366
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2021-03-23 08:44:10 +0300

    add files

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+// Copyright 2020 Konina Tatiana
+#include <iostream>
+#include "ocean.h"
+
+int main() {
+	Ocean* oc = new Ocean;
+	oc->addObjects(3000, ObjType::PREY);
+	oc->addObjects(1500, ObjType::PREDATOR);
+	oc->addObjects(1000, ObjType::STONE);
+	oc->run();
+	system("pause");
+  return 0;
+}
+
+
+
+
+
+
+
