637691e2f6896af7a109145f50f5149633aa5001
Vadukk <Vadik-Tumakov.com@yandex.ru>
2021-03-24 12:19:57 +0300

    1

---
+++ include/predator.h
@@ -0,0 +1,21 @@
+// Copyright 2020 Vadukk
+#ifndef INCLUDE_PREDATOR_H_
+#define INCLUDE_PREDATOR_H_
+#include "../include/object.h"
+class Cell;
+
+class Predator : public Object {
+  friend Cell;
+ private:
+  bool hungry;
+ public:
+  explicit Predator(Cell* c);
+  ~Predator() {}
+  void live();
+  void move();
+  void eat();
+  void makeChild();
+  char getSymbol();
+};
+
+#endif  // INCLUDE_PREDATOR_H_
