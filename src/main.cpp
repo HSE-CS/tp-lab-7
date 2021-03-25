e417c318c5141eaf9bbc971b556de8703905d79c
GlebNeshchetkin <glebneshchetkin@gmail.com>
2021-03-25 19:24:23 +0300

    10Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
-﻿// Copyright GN 2021  
+﻿
+//  Copyright GN 2021
+
+
 #include "../include/ocean.h"
+
+
 int main() {
+
+
   Ocean ocn1;
+
+
   ocn1.generateWorld(1000, 500, 50, 40);
+
+
   ocn1.run();
+
+
 }
