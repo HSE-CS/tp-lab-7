1727a82265a4bc3d45854dfc4232783758489cdf
GlebNeshchetkin <glebneshchetkin@gmail.com>
2021-03-25 18:33:22 +0300

    10Test

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 
 #include "../include/ocean.h"
 
+
 int main() {
-  Ocean ocn;
-  ocn.generateWorld(1000, 500, 50, 40);
-  ocn.run();
-  return 0;
+  Ocean ocn1;
+  ocn1.generateWorld(1000, 500, 50, 40);
+  ocn1.run();
 }
