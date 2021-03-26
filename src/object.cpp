d130548f3b95203797e4ceff2e768c3e8920ca39
dmitrycvetkov2000 <dimacvetkov2000@mail.ru>
2021-03-26 22:57:30 +0300

    cpplint

--- src/object.cpp
+++ src/object.cpp
@@ -3,13 +3,10 @@
 
 #include "cell.h"
 
-#include <cassert>
-
 Object::Object(std::weak_ptr<Cell> cell) : cell { cell } {
 }
 
 void Object::moveToCell(const std::weak_ptr<Cell>& cell) {
-    assert(this->cell.lock()->getObject());
     this->cell.lock()->setObject({});
     this->cell = cell;
     this->cell.lock()->setObject(weak_from_this());
