c38756a6b4ca7b2561ff682b8ee83969f40c6b74
Denis Golovanov <dmgolovanov@edu.hse.ru>
2021-03-26 22:27:32 +0300

    First attempt

---
+++ include/common.h
@@ -0,0 +1,17 @@
+// Copyright 2021 Golovanov
+#ifndef INCLUDE_COMMON_H_
+#define INCLUDE_COMMON_H_
+
+struct Pair {
+    int x;
+    int y;
+};
+
+const int M = 60;
+const int N = 70;
+const int number_objects = 3000;
+
+const int probability_border_up = 60;
+const int probability_border_down = 40;
+
+#endif  // INCLUDE_COMMON_H_
