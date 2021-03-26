da28220070ecdb53aa8fd57815baf40f28e4cb07
eakrayushkina57 <eakrayushkina_1@edu.hse.ru>
2021-03-26 22:44:01 +0300

    b103

--- src/Ocean.cpp
+++ src/Ocean.cpp
@@ -29,7 +29,8 @@ void Ocean::addObjects(unsigned int n, ObjType type) {
         unsigned int y = rand_r() % N;
         if (cells[y][x].getObject())
             continue;
-        else {
+        else 
+        {
             Object * born = nullptr;
             switch (type) {
             case ObjType::STONE:
@@ -46,7 +47,7 @@ void Ocean::addObjects(unsigned int n, ObjType type) {
             cells[y][x].setObject(born);
             stuff.push_back(born);
             n--;
-            }
+        }
     }
 }
 void Ocean::run() {
