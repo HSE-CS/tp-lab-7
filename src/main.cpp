fc156bf4cb9ed3ff35aba41abfe87ae353a1f8ef
NazarovVyacheslav <slavokeru@gmail.com>
2021-03-26 21:38:56 +0300

    first b104

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+// Copyright slavokeru 2021
+#include<iostream>
+#include"ocean.h"
+int main() {
+	Ocean O;
+	O.run();
+	return 0;
+}
---
+++ src/stone.cpp
@@ -0,0 +1,5 @@
+// Copyright slavokeru 2021
+#include"stone.h"
+Stone::Stone() {
+	this->type = ObjType::STONE;
+}
