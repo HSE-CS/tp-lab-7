644e09b31b3283b04685c2805b6cc8a076aa86e7
eakrayushkina57 <eakrayushkina_1@edu.hse.ru>
2021-03-26 23:28:02 +0300

    b103

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 // Copyright 2021 Krayushkina
 #include <iostream>
 #include "../include/Ocean.h"
-using namespace std;
 int main()
 {
-    Ocean *oc = new Ocean;
+    Ocean * oc = new Ocean;
     oc->addObjects(40, ObjType::PREY);
     oc->addObjects(10, ObjType::PREDATOR);
     oc->addObjects(10, ObjType::STONE);
