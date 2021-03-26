6ca4a0d3f8545c5eefd59f09e4e768eeda713f20
DenisKabanov <dskabanov@edu.hse.ru>
2021-03-26 20:58:28 +0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 // Copyright 26.03.21 DenisKabanov
 
 #include <ocean.h>
+
 int main() {
-  std::cout << "Ocean: " << std::endl;
+  std::cout << "Project: " << std::endl;
   Ocean ocean(30, 70, 20, 30, 20);
   ocean.run();
   return 0;
