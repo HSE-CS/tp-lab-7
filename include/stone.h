f6eae8682b9f957e297ef9aa606af9154f190beb
TimurZaytsev <71225350+TimurZaytsev@users.noreply.github.com>
2021-03-27 01:37:59 +0300

    Update stone.h

--- include/stone.h
+++ include/stone.h
@@ -1,7 +1,7 @@
 // Copyright TimurZaytsev 2021
 #ifndef INCLUDE_STONE_H_
 #define INCLUDE_STONE_H_
-#include "object.h"
+#include "../include/object.h"
 class Stone : public Object {
  public:
   Stone();
