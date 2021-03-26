99b833b35fa81d2a549fe2105d51961b04832677
NoName <ihackit595@gmail.com>
2021-03-26 20:06:59 +0300

    update object files

--- include/object.h
+++ include/object.h
@@ -1,3 +1,4 @@
+// Copyright 2021 Bogomazov
 #ifndef _OBJECT_H_
 #define _OBJECT_H_
 
@@ -14,11 +15,16 @@ class Cell;
 class Object {
  protected:
     Cell *cell;
+    char type;
  public:
     explicit Object(Cell * = nullptr);
     virtual ~Object();
     virtual void live() = 0;
     void setCell(Cell*);
+
+    Cell* getCell();
+    char getType();
+    void setType(char type);
 };
 
 #endif  //   _OBJECT_H_
