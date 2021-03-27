9323a765f3cda2d1c270ac0ee74139e3a1cb3da9
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2021-03-27 03:21:27 +0300

    Update common.h

--- include/common.h
+++ include/common.h
@@ -3,14 +3,12 @@
 #ifndef INCLUDE_COMMON_H_
 #define INCLUDE_COMMON_H_
 
-typedef size_t coord_t;
-
 typedef struct Coords {
-  coord_t x;  // 0..M-1
-  coord_t y;  // 0..N-1
+  int x;  // 0..M-1
+  int y;  // 0..N-1
 } coords;
 
-const size_t N = 50;
-const size_t M = 200;
+const int N = 50;
+const int M = 200;
 
 #endif  // INCLUDE_COMMON_H_
