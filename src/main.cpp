1e4cf83a05ac69f255b73e3d411a97f49a452dd0
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2021-03-27 03:01:00 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 // Copyright 2021 ArinaMonicheva
 
-#include "predator.h"
+#include "../include/predator.h"
 
 int main() {
   time_t* n = NULL;
@@ -15,7 +15,7 @@ int main() {
   int arr[3] = { stones, preys, predators };
   int size = 0;
   std::vector < std::vector <coords> > availableCoords(
-   N, std::vector<coords>());
+    N, std::vector<coords>());
 
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < M; j++) {
